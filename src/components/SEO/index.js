ec58ccafdff375bfea0b0bfa20ca54398d0cf587
subject026 <sudobuildspeakers@gmail.com>
2020-02-29 00:51:01 +0000

    added site title static query

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { useStaticQuery } from "gatsby";
 
 export default ({ title }) => {
-  return <Helmet title={title} />;
+  const data = useStaticQuery(graphql`
+    query {
+      wordpress {
+        node {
+          id
+        }
+        allSettings {
+          generalSettingsTitle
+          generalSettingsDescription
+        }
+      }
+    }
+  `);
+  const { generalSettingsTitle } = data.wordpress.allSettings;
+  return <Helmet title={`${generalSettingsTitle} | ${title}`} />;
 };
