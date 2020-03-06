e1962c27dcdeacabcf69a3bb2b745889e652a023
subject026 <sudobuildspeakers@gmail.com>
2020-03-06 20:36:16 +0000

    header styles

--- src/components/Header/header.css.js
+++ src/components/Header/header.css.js
@@ -9,6 +9,7 @@ export const HeaderInner = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
 export const SiteTitle = styled.span`
@@ -17,10 +18,33 @@ export const SiteTitle = styled.span`
 `;
 
 export const Nav = styled.nav`
-  ul {
-    list-style-type: none;
-  }
+  margin: 0;
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  z-index: 10;
   li {
-    display: inline-block;
+    display: block;
+    margin: 0;
+  }
+  a {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    padding: 0 20px;
+  }
+  @media (min-width: 600px) {
+    position: static;
+    height: 100%;
+    ul {
+      list-style-type: none;
+      height: 100%;
+    }
+    li {
+      display: inline-block;
+      height: 100%;
+    }
   }
 `;
