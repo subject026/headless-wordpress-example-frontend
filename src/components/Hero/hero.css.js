01640416118ffef2bb13d91af5280914d0b3ab09
subject026 <sudobuildspeakers@gmail.com>
2020-03-07 18:29:34 +0000

    removed hero fade in experiment

--- src/components/Hero/hero.css.js
+++ src/components/Hero/hero.css.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import BackgroundImage from "gatsby-background-image";
 
 export const Hero = styled.section`
@@ -13,35 +13,6 @@ export const Hero = styled.section`
 
 export const StyledBackgroundImage = styled(BackgroundImage)`
   height: 100%;
-  /* opacity: 0; */
-  transition: 0.5s;
-  /* ${({ state }) => {
-    let styles;
-    switch (state) {
-      case "entering":
-        styles = "opacity: 0;";
-        console.log("\n\nstate: ", state);
-        console.log("\n\ncss: ", styles);
-        return styles;
-      case "entered":
-        styles = "opacity: 1;";
-        console.log("\n\nstate: ", state);
-        console.log("\n\ncss: ", styles);
-        return styles;
-      case "exiting":
-        styles = "opacity: 1;";
-        console.log("\n\nstate: ", state);
-        console.log("\n\ncss: ", styles);
-        return styles;
-      case "exited":
-        styles = "opacity: 0;";
-        console.log("\n\nstate: ", state);
-        console.log("\n\ncss: ", styles);
-        return styles;
-      default:
-        return null;
-    }
-  }} */
 `;
 
 export const HeroInner = styled.div`
@@ -49,31 +20,3 @@ export const HeroInner = styled.div`
   display: flex;
   align-items: center;
 `;
-
-export const H1 = styled.h1`
-  /* opacity: 0;
-  transition: opacity 800ms ease-in;
-  ${({ state }) => {
-    switch (state) {
-      case "entering":
-        return css`
-          opacity: 0;
-        `;
-      case "entered":
-        return css`
-          opacity: 1;
-        `;
-      case "exiting":
-        return css`
-          opacity: 1;
-        `;
-      case "exited":
-        return css`
-          opacity: 0;
-        `;
-      default:
-        return null;
-    }
-  }} */
-`;
-export const H3 = styled.h1``;
